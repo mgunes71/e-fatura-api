dd6efe88dd77a54e3c9c65dfd9afe550459f58f0
Murat GÜNEŞ <mgnes9571@gmail.com>
2021-11-25 23:34:44 +0300

    pj1

--- src/configuration/ormconfig.ts
+++ src/configuration/ormconfig.ts
@@ -1,8 +1,14 @@
-import {SqliteConnectionOptions} from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 
 const config: SqliteConnectionOptions = {
-    type: 'sqlite',
-    database: 'db',
-    entities:
+  type: 'sqlite',
+  database: 'db',
+  entities: [
+    // 'dist/entitity/user.entitiy.js',
+    // 'dist/entitity/expense.entitiy.js',
+    // 'dist/entitity/income.entitity.js',
+  ],
+  synchronize: true,
 };
 
+export default config;
